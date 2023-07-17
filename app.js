e44980c0a47fac595d9a8069ff469a05ad765d2a
catherine-newman <3197941+catherine-newman@users.noreply.github.com>
2023-07-17 09:47:48 +0100

    allow cross origin resource sharing

--- app.js
+++ app.js
@@ -1,9 +1,12 @@
 const express = require("express");
 const apiRouter = require("./routes/api-router");
+const cors = require("cors");
+
 const app = express();
 app.use(express.json());
-
+app.use(cors());
 app.use("/api", apiRouter);
+
 
 app.all("*", (req, res) => {
     res.status(404).send({ status: 404, msg: "Not Found"});
